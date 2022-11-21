8caf52eb613d45fa48a0b50206f92abea52be990
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-11-21 12:43:34 +0100

    fix: nugets

--- ResultCommander/DependancyInjectionExtensions.cs
+++ ResultCommander/DependancyInjectionExtensions.cs
@@ -143,7 +143,7 @@ public static class DependancyInjectionExtensions
             
         foreach (var attribute in decoratorAttributes.OrderBy(x => x.RegistrationOrder))
         {
-            if (attribute.Decorator.ShouldSkipRegistration<ISkipHandlerRegistrationAttribute>())
+            if (attribute.Decorator.ShouldSkipRegistration<ISkipDecoratorRegistrationAttribute>())
                 continue;
             
             if (attribute.Decorator.IsGenericType && attribute.Decorator.IsGenericTypeDefinition)
